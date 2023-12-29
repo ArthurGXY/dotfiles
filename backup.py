3be79b6dfe92613b0377efc6aa88773cbfa6aa87
Guo Xiaoyao <arthurgxy@outlook.com>
2023-12-29 11:13:31 -0500

    fixed repeated directory

--- backup.py
+++ backup.py
@@ -8,6 +8,7 @@ import os
 #   1 list of sources and 
 #   1 list of destinations 
 # in `sources` and `dests`
+root = os.getcwd()
 categories: List[AnyStr] = [] 
 sources: List[List[AnyStr]] = [] # sources and dests are 1-to-1 mapping
 dests: List[List[AnyStr]] = []
@@ -23,9 +24,6 @@ def read_list(list_path: PathLike):
     # reads the 'list' file in the folder to track which folders are managed.
     categories_count = 0
     # resets the lists in the script
-    categories = []
-    sources = []
-    dests = []
     
     print("reading list.")
     
@@ -41,20 +39,24 @@ def read_list(list_path: PathLike):
                     dests.append([])
                 else:
                     src, dest = [x.strip() for x in line.split(" ")]
-                    sources[categories_count - 1].append(src)
-                    dests[categories_count - 1].append(dest)
+                    sources[categories_count - 1].append(os.path.expanduser(src))
+                    dests[categories_count - 1].append(os.path.expanduser(dest))
                     
     print(f"categories: {categories}\nsources: {sources}\ndestinations: {dests}")
                     
 
 def backup():
     # copy files according to the 3 managed lists
+    
     categories_count = categories.__len__()
+    print(f"Entered backup, categories: {categories_count}")
     for i in range(categories_count):
         print(f"Backing up category {categories[i]}")
-        for src, dest in zip(sources, dests):
+        for src, dest in zip(sources[i], dests[i]):
             
-            copy_cmd = f"cp {src} {dest} -r"
+            parent_dir = os.path.dirname(dest)
+            
+            copy_cmd = f"cp {src} {parent_dir if parent_dir else root} -r"
             
             print(f"Copying from {src} -> {dest}")
             
@@ -62,16 +64,23 @@ def backup():
                 print(f"Source {src} does not exist.")
                 
             elif os.path.isfile(src):
+                ensure_dir(parent_dir) if parent_dir else None 
                 os.system(copy_cmd)
                 
             elif os.path.isdir(src):
                 ensure_dir(dest)
+                if os.path.isdir(dest): 
+                    # copy each file into dest instead of the directory.
+                    copy_cmd = f"cp {src}/* {parent_dir if parent_dir else root} -r"
                 os.system(copy_cmd)
                 
             else:
                 print("Unknown situation happened.")
                 continue
 
+def restore():
+    # todo: restore backup files
+    pass
 
 def main():
     read_list("./list")
